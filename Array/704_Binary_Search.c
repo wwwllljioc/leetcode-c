ff6ddf3c145af8e82ecfafa6816e0c5d4f8b5f7b
niXman <14773442349@163.com>
2026-05-11 18:39:31 +0800

    添加注释“

--- Array/704_Binary_Search.c
+++ Array/704_Binary_Search.c
@@ -1,13 +1,13 @@
 int search(int* nums, int numsSize, int target) {
     int left,right;int mid;
     left = 0; right = numsSize - 1;
-    while(left <= right){
+    while(left <= right){           //等于为了解决[5]一种元素情况
         mid=(left + right)/2;
         if(nums[mid]==target){
             return mid;
         }
         else if(nums[mid] > target){
-            right = mid - 1;
+            right = mid - 1;        //让区间缩小，以防止进入死循环如[5],或者[2,4]这种类型
             continue;
         }
         else if(nums[mid] < target){
